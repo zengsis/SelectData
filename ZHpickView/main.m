9a7e74112fe7936b67d8466eb82d9129f3b23cc7
zengsis <si@zengsi.local>
2015-12-27 17:46:15 +0800

    select data from array

---
+++ ZHpickView/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ZHpickView
+//
+//  Created by liudianling on 14-11-18.
+//  Copyright (c) 2014年 赵恒志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZHAppDelegate class]));
+    }
+}
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com 
